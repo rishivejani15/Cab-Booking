0341573576aff0d00e6c8756d3d77321117b5bef
Rishi Vejani <123580547+15Rishi@users.noreply.github.com>
2023-07-29 15:36:16 +0530

    Add files via upload

---
+++ Cab.java
@@ -0,0 +1,92 @@
+import java.util.*;
+class Cab
+{
+	public static void main (String args[])
+	{
+		Scanner sc = new Scanner (System.in);
+
+		System.out.println("1 for Day");
+		System.out.println("2 for Night");
+		System.out.println("3 for Full Day");
+		System.out.print("Choose an option: ");
+		int option = sc.nextInt();
+		int fare = 0;
+		System.out.println("Welcome to Uber !!!");
+		System.out.print("Enter distance in km: ");
+		double distance = sc.nextDouble();
+		switch(option)
+		{
+			case 1:
+				System.out.print("Enter waiting time in min: ");
+				int time1 = sc.nextInt();
+				if (distance <= 1.5)
+				{
+					fare += 29;
+				}
+				else if (distance > 1.5)
+				{
+					fare += 29;
+					distance -= 1.5;
+					fare += (distance*10 )*2;
+				}
+				fare += time1*2;
+				System.out.print("Total Fare is: " + fare);
+				break;
+			case 2:
+				System.out.print("Enter waiting time in min: ");
+				int time2 = sc.nextInt();
+				if (distance <= 1.5)
+				{
+					fare += 35;
+				}
+				else if (distance > 1.5)
+				{
+					fare += 35;
+					distance -= 1.5;
+					fare += (distance*10)*3;
+				}
+				fare += time2*3;
+				System.out.print("Total Fare is: " + fare);
+				break;
+			case 3:
+				System.out.println("Press 1 for AC");
+				System.out.println("Press 2 for NON - AC");
+				int choice = sc.nextInt();
+				switch(choice)
+				{
+					case 1:
+						if (distance <= 200)
+						{
+							fare += 7000;
+						}
+						else if (distance > 200)
+						{
+							fare += 7000;
+							distance -= 200	;
+							fare += (distance*30);
+						}		
+						System.out.print("Total Fare is: " + fare);
+						break;
+					case 2:
+						if (distance <= 200)
+						{
+							fare += 5000;
+						}
+						else if (distance > 200)
+						{
+							fare += 5000;
+							distance -= 200	;
+							fare += (distance*20);
+						}
+						System.out.print("Total Fare is: " + fare);
+						break;
+					
+					default:
+						System.out.println("Invalid Input !!!");
+				}
+				break;
+			default:
+				System.out.println("Invalid Input !!!");
+		}
+	}
+}
